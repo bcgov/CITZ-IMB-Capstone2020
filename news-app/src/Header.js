3fc334a27e7fb6d2b9f4a590bf9b9dedd7b3d238
Josh Dueck <josh.dueck@hotmail.com>
2020-06-04 14:54:02 -0700

    Added Header and Footer files

---
+++ news-app/src/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+    return (
+      // Contents of the header
+      <div id="header">
+          <div id="header-top">
+            <a href="http://localhost:3000/">
+              <img id="bc-gov-logo" src={require("./includes/gov_bc_logo.svg")} alt="bc-gov-logo" title="B.C. News Site" />
+            </a>
+          </div>
+          <div id="header-bottom">
+              <h2>BC Gov News</h2>
+          </div>
+      </div>
+    );
+  }
+  
+  export default Header;
