e813bbb4940c5d35f2169cf32e594c5e35b098d6
Josh Dueck <josh.dueck@hotmail.com>
2020-06-08 14:20:34 -0700

    Merged body content into app

--- news-app/src/App.js
+++ news-app/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Header from './Header.js';
 import Footer from './Footer.js';
+import Navbar from './Navbar.js';
+import FetchLatestNews from './FetchLatestNews.js';
 //import './App.css';
 
 function App() {
   return (
     <div>
-      <Header />,
+      <Header />
+      <Navbar />
+      <FetchLatestNews />
       <Footer />
      
     </div>
---
+++ news-app/src/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Navbar.css';
+
+function Navbar() {
+    return (
+      // Contents of the Navbar
+      <div id="navbar">
+
+      </div>
+    );
+  }
+  
+  export default Navbar;
