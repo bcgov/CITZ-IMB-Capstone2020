2783cff779032baa0c5def0638da2c8baf385c14
Murphy316 <liqi316@live.com>
2020-07-07 17:15:43 -0700

    add dark mode

--- src/views/App.js
+++ src/views/App.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-06-23 14:59:48
- * @LastEditTime: 2020-07-07 15:40:57
+ * @LastEditTime: 2020-07-07 17:15:05
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \CITZ-IMB-Capstone2020 - xordpe-dev\src\views\App.js
@@ -12,18 +12,36 @@ import Footer from './Footer.js';
 import Navbar from './Navbar.js';
 import FetchLatestNews from './FetchLatestNews.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
+// add .Dark mode
+import {ThemeProvider} from "styled-components";
+import { GlobalStyles } from "./GlobalStyles.js";
+import { lightTheme, darkTheme } from "./Themes.js"
+import { UseDarkMode } from "./UseDarkMode.js"
+import Toggler from "./Toggler.js"
 
-function App() { 
+function App() {
+  const [theme, themeToggler, mountedComponent] = UseDarkMode();
+  const themeMode = theme === 'light' ? lightTheme : darkTheme;
+
+  //The highlighted code is the only one added to UseDarkMode.js
+  if(!mountedComponent) return <div/>
+  
   return (
+    <ThemeProvider theme={themeMode}>
+      <>
+      <GlobalStyles/>
       <div>
         <Header />
         <Navbar />
+        <Toggler theme={theme} toggleTheme={themeToggler} />
         <FetchLatestNews />
         <br/><br/>
         <Footer />
-    
       </div>
+      </>
+    </ThemeProvider>
   );
 }
 
 export default App;
+
