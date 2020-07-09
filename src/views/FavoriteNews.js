09e279009840721100861b81632aa21a43d33762
Josh Dueck <josh.dueck@hotmail.com>
2020-07-08 20:26:50 -0700

    Moved AntSwitch out of FavoriteNews

--- src/views/FavoriteNews.js
+++ src/views/FavoriteNews.js
@@ -14,6 +14,7 @@ import FormGroup from '@material-ui/core/FormGroup';
 import Switch from '@material-ui/core/Switch';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
+import AntSwitch from './AntSwitch.js';
 
 export default function FetchLatestNews2() {
 
@@ -39,7 +40,7 @@ const [url, setUrl] = useState(`https://news.api.gov.bc.ca/api/Posts/${key}?api-
    const fetchData = async () => {
      const result = await axios(url);
      setData(result.data);
-     
+
    };
 
    fetchData();
@@ -127,37 +128,3 @@ const [url, setUrl] = useState(`https://news.api.gov.bc.ca/api/Posts/${key}?api-
    </div>
  );
 }
-
-const AntSwitch = withStyles((theme) => ({
- root: {
-   width: 28,
-   height: 16,
-   padding: 0,
-   display: 'flex',
- },
- switchBase: {
-   padding: 2,
-   color: theme.palette.grey[500],
-   '&$checked': {
-     transform: 'translateX(12px)',
-     color: theme.palette.common.white,
-     '& + $track': {
-       opacity: 1,
-       backgroundColor: theme.palette.primary.main,
-       borderColor: theme.palette.primary.main,
-     },
-   },
- },
- thumb: {
-   width: 12,
-   height: 12,
-   boxShadow: 'none',
- },
- track: {
-   border: `1px solid ${theme.palette.grey[500]}`,
-   borderRadius: 16 / 2,
-   opacity: 1,
-   backgroundColor: theme.palette.common.white,
- },
- checked: {},
-}))(Switch);
