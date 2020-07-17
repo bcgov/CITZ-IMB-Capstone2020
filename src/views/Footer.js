1433180dec6327896069e115ec3d0270338c724a
Murphy316 <liqi316@live.com>
2020-07-16 17:28:13 -0700

    more styles for footer

--- src/views/Footer.js
+++ src/views/Footer.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-07-15 12:01:20
- * @LastEditTime: 2020-07-16 15:32:46
+ * @LastEditTime: 2020-07-16 17:24:37
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \CITZ-IMB-Capstone2020\src\views\Footer.js
@@ -16,23 +16,23 @@ function Footer() {
       <div class="contain">
       <div class="col">
       <div />
-        <a id="button" href="https://www2.gov.bc.ca/gov/content/home/disclaimer">Disclaimer</a>
+        <a id="button" target="_blank" rel="noopener noreferrer" href="https://www2.gov.bc.ca/gov/content/home/disclaimer">Disclaimer</a>
       </div>
       <div class="col">
       <div />
-        <a id="button" href="https://www2.gov.bc.ca/gov/content/home/copyright">Copyright</a>
+        <a id="button" target="_blank" rel="noopener noreferrer" href="https://www2.gov.bc.ca/gov/content/home/copyright">Copyright</a>
       </div>
       <div class="col">
       <div />
-        <a id="button" href="https://www2.gov.bc.ca/gov/content/home/privacy">Privacy</a>
+        <a id="button" target="_blank" rel="noopener noreferrer" href="https://www2.gov.bc.ca/gov/content/home/privacy">Privacy</a>
       </div>
       <div class="col">
       <div />
-        <a id="button" href="https://www2.gov.bc.ca/gov/content/home/accessible-government">Accessibility</a>
+        <a id="button" target="_blank" rel="noopener noreferrer" href="https://www2.gov.bc.ca/gov/content/home/accessible-government">Accessibility</a>
       </div>
       <div class="col">
       <div />
-        <a id="button" href="https://github.com/bcgov/CITZ-IMB-Capstone2020/issues/new">Feedback</a>
+        <a id="button" target="_blank" rel="noopener noreferrer" href="https://github.com/bcgov/CITZ-IMB-Capstone2020/issues/new">Feedback</a>
       </div>
     </div>
     </div>
